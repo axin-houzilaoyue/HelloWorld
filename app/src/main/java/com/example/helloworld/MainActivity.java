996f166cca9e29a651ed7a760482a88cb20cdffa
houzilaoyue <1977494112@qq.com>
2020-03-21 16:39:54 +0800

    第一次尝试
    Signed-off-by: houzilaoyue <1977494112@qq.com>

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        Button btn1 = findViewById(R.id.btn1);
+//        final TextView tv1 = findViewById(R.id.tv1);
+//
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v){
+//                tv1.setText("What a awful day!");
+//            }
+//        });
+    }
+}
