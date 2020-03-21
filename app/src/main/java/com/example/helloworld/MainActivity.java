c3a4a3286caca6178c9d7cf7402799e33d19a756
houzilaoyue <1977494112@qq.com>
2020-03-21 17:26:10 +0800

    第一次提交到仓库
    Signed-off-by: houzilaoyue <1977494112@qq.com>

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 //import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,15 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        Button btn1 = findViewById(R.id.btn1);
-//        final TextView tv1 = findViewById(R.id.tv1);
-//
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v){
-//                tv1.setText("What a awful day!");
-//            }
-//        });
+
+        Button btn1 = findViewById(R.id.button2);
+        final TextView tv1 = findViewById(R.id.textView2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Log.d("123:","按钮被按了！");
+                tv1.setText("What a awful day!");
+            }
+        });
     }
 }
