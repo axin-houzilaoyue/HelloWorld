59ac752d1daa1d0773d965ef8953416ad8c689b6
houzilaoyue <1977494112@qq.com>
2020-03-22 10:56:32 +0800

    1
    Signed-off-by: houzilaoyue <1977494112@qq.com>

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 //import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
